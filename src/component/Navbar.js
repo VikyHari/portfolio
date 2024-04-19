cb39b9c5a608ad9ffe8bb6b75d60e7f33926af3c
VikyHari <vikyhari321@gmail.com>
2024-04-19 14:41:52 +0530

    experience bar completed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function NavbarComponent() {
   return (
     <div>
-        <Navbar className='navbar' >
+      <Navbar className="navbar">
         <Container>
-        <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" >Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-          </Nav>
+          <Navbar.Brand as={Link} to="/">
+            Navbar
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-center">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/skills">
+                Skills
+              </Nav.Link>
+              <Nav.Link as={Link} to="/projects">
+                Projects
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
-  )
+  );
 }
 
-export default NavbarComponent
+export default NavbarComponent;
--- src/container/Homepage/Home.js
+++ src/container/Homepage/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import background from "../../assests/images/background.jpg";
+import javaScript from "../../assests/images/javascript.png";
+import html from "../../assests/images/html.png";
+import css from "../../assests/images/css.png";
+import react from "../../assests/images/react.png";
+
 import { motion } from "framer-motion";
 
 // background-color: #f1f1f1;
@@ -77,16 +82,30 @@ function Home() {
           "linear-gradient(90deg, rgba(51,28,82,1) 48%, rgba(41,28,58,1) 100%)",
         flex: 1,
         height: "650px",
-        padding:'16px'
+        padding: "16px",
       }}
     >
       <div
         className="Introduction"
-        style={{ display: "flex", flexDirection: "row", gap:'20px', border:'1px solid white' }}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "20px",
+          // border: "1px solid white",
+        }}
       >
-        <div style={{ width: "50%" ,  display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', gap:'10px'}}>
+        <div
+          style={{
+            width: "50%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "10px",
+          }}
+        >
           <motion.h5
-          style={{fontSize:'40px'}}
+            style={{ fontSize: "40px" }}
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ ease: "easeInOut", duration: 1.5 }}
@@ -94,15 +113,15 @@ function Home() {
             Hello ,
           </motion.h5>
           <motion.h1
-           style={{fontSize:'60px', color:'#DCCFED'}}
+            style={{ fontSize: "60px", color: "#DCCFED" }}
             initial={{ opacity: 0, x: 100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ ease: "easeInOut", duration: 1.5 }}
           >
-          I'm a Web Developer
+            I'm a Web Developer
           </motion.h1>
           <motion.h4
-           style={{fontSize:'45px'}}
+            style={{ fontSize: "45px" }}
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ ease: "easeInOut", duration: 1.5 }}
@@ -111,13 +130,97 @@ function Home() {
           </motion.h4>
         </div>
         <motion.div
-         style={{width:'50%',display:'flex', alignItems:'center', justifyContent:'center'}}
+          style={{
+            width: "50%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ ease: "easeInOut", duration: 1.5 }}
         >
-          <img src={background} alt="Your Image" style={{ maxWidth: "80%", }} />
+          <img src={background} alt="Your Image" style={{ maxWidth: "80%" }} />
         </motion.div>
+      </div>
+      <div className="experience">
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "#291c3a",
+            padding: "8px",
+            marginTop:'10px'
+          }}
+        >
+          <div
+            style={{ width: "20%", display: "flex", justifyContent: "center" }}
+          >
+            <motion.p
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ ease: "easeInOut", duration: 1.5 }}
+              style={{ fontSize: "20px" }}
+            >
+              <span style={{ fontSize: "50px" }}>3</span> years of Experience{" "}
+            </motion.p>
+          </div>
+          <div
+            style={{ width: "30%", display: "flex", justifyContent: "center" }}
+          >
+            <motion.p
+              style={{ fontSize: "20px" }}
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ ease: "easeInOut", duration: 1.5 }}
+            >
+              <span style={{ fontSize: "50px" }}>12</span> Projects Completed
+              Around the World
+            </motion.p>
+          </div>
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ ease: "easeInOut", duration: 1.5 }}
+            style={{
+              width: "50%",
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
+            <motion.img
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1.5 }}
+              src={javaScript}
+              alt="Your Image"
+              style={{ Width: "90px", height: "90px" }}
+            />
+            <motion.img
+              initial={{opacity: 0, y:100}}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ ease: "easeInOut", duration: 1.5 }}
+              src={html}
+              alt="Your Image"
+              style={{ Width: "90px", height: "90px" }}
+            />
+            <motion.img
+              initial={{opacity: 0, y:-100}}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ ease: "easeInOut", duration: 1.5 }}
+              src={css}
+              alt="Your Image"
+              style={{ Width: "90px", height: "90px" }}
+            />
+            <motion.img
+              initial={{opacity: 0, x:100}}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1.5 }}
+              src={react}
+              alt="Your Image"
+              style={{ Width: "90px", height: "90px" }}
+            />
+          </motion.div>
+        </div>
       </div>
     </div>
   );
